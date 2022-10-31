7680f390c14c14f52fe9da95dad9ed636ae5c64f
DanielXavier114 <danielsxavier7@gmail.com>
2022-10-31 10:08:11 +0000

    alteracoes

--- PROJETO-RAS/src/main.java
+++ PROJETO-RAS/src/main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class main {
+public class main{
+    public static void main(String[] args) {
+        System.out.println("Bem vindos ao RasBet!");
+
+        // Inicializar a base de dados
+        BaseDados bd = new BaseDados();
+
+        // Inicializar o menu
+        Menu menu = new Menu(bd);
+        menu.run();
+
+        // Fechar a base de dados
+        bd.close();
+
+        System.out.println("Obrigado por utilizar o RasBet!");
+
+        
+    }
 }
