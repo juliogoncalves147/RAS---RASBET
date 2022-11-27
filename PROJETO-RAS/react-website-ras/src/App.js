1b446b1b1bb75d4e0cace4c523451bd3da8e4b5c
DanielXavier114 <danielsxavier7@gmail.com>
2022-11-27 16:06:23 +0000

    react

---
+++ PROJETO-RAS/react-website-ras/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact />
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;
